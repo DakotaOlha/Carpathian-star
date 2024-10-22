1e9ad7127615aa41dc619c2c9e3c05362576acba
BlautaVi <blautavika@gmail.com>
2024-10-22 19:20:56 +0300

    Added background

--- Carpathian_star/Form1.Designer.cs
+++ Carpathian_star/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SeaGreen;
+            button1.Location = new Point(380, 196);
+            button1.Name = "button1";
+            button1.Size = new Size(20, 18);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.SeaGreen;
+            button2.Location = new Point(162, 351);
+            button2.Name = "button2";
+            button2.Size = new Size(20, 18);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SeaGreen;
+            button3.Location = new Point(351, 355);
+            button3.Name = "button3";
+            button3.Size = new Size(20, 18);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.SeaGreen;
+            button4.Location = new Point(364, 564);
+            button4.Name = "button4";
+            button4.Size = new Size(20, 18);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.SeaGreen;
+            button5.Location = new Point(642, 572);
+            button5.Name = "button5";
+            button5.Size = new Size(20, 18);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.SeaGreen;
+            button6.Location = new Point(700, 447);
+            button6.Name = "button6";
+            button6.Size = new Size(20, 18);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.SeaGreen;
+            button7.Location = new Point(807, 370);
+            button7.Name = "button7";
+            button7.Size = new Size(20, 18);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.SeaGreen;
+            button8.Location = new Point(700, 212);
+            button8.Name = "button8";
+            button8.Size = new Size(20, 18);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1004, 776);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
     }
 }
