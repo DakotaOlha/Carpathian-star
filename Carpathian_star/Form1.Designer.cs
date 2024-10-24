a669fa657f9b07ef68de2328ea4bb59cfde9cd5e
Dakota_Olh <ochaiyk2006@gmail.com>
2024-10-24 20:05:04 +0300

    -

--- Carpathian_star/Form1.Designer.cs
+++ Carpathian_star/Form1.Designer.cs
@@ -43,9 +43,10 @@
             // 
             Store1.BackColor = Color.SeaGreen;
             Store1.ForeColor = SystemColors.ControlLight;
-            Store1.Location = new Point(380, 196);
+            Store1.Location = new Point(475, 245);
+            Store1.Margin = new Padding(4, 4, 4, 4);
             Store1.Name = "Store1";
-            Store1.Size = new Size(20, 18);
+            Store1.Size = new Size(25, 22);
             Store1.TabIndex = 0;
             Store1.UseVisualStyleBackColor = false;
             Store1.MouseEnter += button1_MouseEnter;
@@ -54,73 +55,80 @@
             // Store2
             // 
             Store2.BackColor = Color.SeaGreen;
-            Store2.Location = new Point(162, 351);
+            Store2.Location = new Point(202, 439);
+            Store2.Margin = new Padding(4, 4, 4, 4);
             Store2.Name = "Store2";
-            Store2.Size = new Size(20, 18);
+            Store2.Size = new Size(25, 22);
             Store2.TabIndex = 1;
             Store2.UseVisualStyleBackColor = false;
             // 
             // Store3
             // 
             Store3.BackColor = Color.SeaGreen;
-            Store3.Location = new Point(351, 355);
+            Store3.Location = new Point(439, 444);
+            Store3.Margin = new Padding(4, 4, 4, 4);
             Store3.Name = "Store3";
-            Store3.Size = new Size(20, 18);
+            Store3.Size = new Size(25, 22);
             Store3.TabIndex = 2;
             Store3.UseVisualStyleBackColor = false;
             // 
             // Store4
             // 
             Store4.BackColor = Color.SeaGreen;
-            Store4.Location = new Point(364, 564);
+            Store4.Location = new Point(455, 705);
+            Store4.Margin = new Padding(4, 4, 4, 4);
             Store4.Name = "Store4";
-            Store4.Size = new Size(20, 18);
+            Store4.Size = new Size(25, 22);
             Store4.TabIndex = 3;
             Store4.UseVisualStyleBackColor = false;
             // 
             // Store5
             // 
             Store5.BackColor = Color.SeaGreen;
-            Store5.Location = new Point(642, 572);
+            Store5.Location = new Point(802, 715);
+            Store5.Margin = new Padding(4, 4, 4, 4);
             Store5.Name = "Store5";
-            Store5.Size = new Size(20, 18);
+            Store5.Size = new Size(25, 22);
             Store5.TabIndex = 4;
             Store5.UseVisualStyleBackColor = false;
             // 
             // Store6
             // 
             Store6.BackColor = Color.SeaGreen;
-            Store6.Location = new Point(700, 447);
+            Store6.Location = new Point(875, 559);
+            Store6.Margin = new Padding(4, 4, 4, 4);
             Store6.Name = "Store6";
-            Store6.Size = new Size(20, 18);
+            Store6.Size = new Size(25, 22);
             Store6.TabIndex = 5;
             Store6.UseVisualStyleBackColor = false;
             // 
             // Store7
             // 
             Store7.BackColor = Color.SeaGreen;
-            Store7.Location = new Point(807, 370);
+            Store7.Location = new Point(1009, 462);
+            Store7.Margin = new Padding(4, 4, 4, 4);
             Store7.Name = "Store7";
-            Store7.Size = new Size(20, 18);
+            Store7.Size = new Size(25, 22);
             Store7.TabIndex = 6;
             Store7.UseVisualStyleBackColor = false;
             // 
             // Store8
             // 
             Store8.BackColor = Color.SeaGreen;
-            Store8.Location = new Point(700, 212);
+            Store8.Location = new Point(875, 265);
+            Store8.Margin = new Padding(4, 4, 4, 4);
             Store8.Name = "Store8";
-            Store8.Size = new Size(20, 18);
+            Store8.Size = new Size(25, 22);
             Store8.TabIndex = 7;
             Store8.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(1004, 776);
+            ClientSize = new Size(1255, 970);
             Controls.Add(Store8);
             Controls.Add(Store7);
             Controls.Add(Store6);
@@ -130,6 +138,7 @@
             Controls.Add(Store2);
             Controls.Add(Store1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(4, 4, 4, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
--- Carpathian_star/Form1.cs
+++ Carpathian_star/Form1.cs
@@ -5,12 +5,9 @@ namespace Carpathian_star
         public Form1()
         {
             InitializeComponent();
-            this.Text = "";
-            this.ShowIcon = false;
-            var screen = Screen.FromControl(this);
-            this.Top = screen.Bounds.Height / 2 - this.Height / 2;
-            this.Left = screen.Bounds.Width / 2 - this.Width / 2;
         }
+
+
 
         private void button1_MouseEnter(object sender, EventArgs e)
         {
@@ -19,6 +16,15 @@ namespace Carpathian_star
         private void button1_MouseLeave(object sender, EventArgs e)
         {
             Store1.BackColor = Color.SeaGreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "";
+            this.ShowIcon = false;
+            var screen = Screen.FromControl(this);
+            this.Top = screen.Bounds.Height / 2 - this.Height / 2;
+            this.Left = screen.Bounds.Width / 2 - this.Width / 2;
         }
     }
 }
