a541cfb996a8ee183986e3eac2f7009b46db6d14
Dakota_Olh <ochaiyk2006@gmail.com>
2024-10-24 16:38:30 +0300

    Добавила розташування екрану та забрала іконку

--- Carpathian_star/Form1.cs
+++ Carpathian_star/Form1.cs
@@ -5,6 +5,21 @@ namespace Carpathian_star
         public Form1()
         {
             InitializeComponent();
+            this.Text = "";
+            this.ShowIcon = false;
+            var screen = Screen.FromControl(this);
+            this.Top = screen.Bounds.Height / 2 - this.Height / 2;
+            this.Left = screen.Bounds.Width / 2 - this.Width / 2;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.GreenYellow;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.SeaGreen;
         }
     }
 }
