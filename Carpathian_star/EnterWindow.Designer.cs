b9e27ae4fc6dde6f80d5138d1c03107e64adda6c
BlautaVi <blautavika@gmail.com>
2024-10-25 17:29:38 +0300

    Some new changes

--- Carpathian_star/EnterWindow.Designer.cs
+++ Carpathian_star/EnterWindow.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EnterWindow));
             Start = new Button();
-            Carpathian = new Label();
-            star = new Label();
             Profile = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // Start
@@ -50,40 +50,29 @@
             Start.TabIndex = 0;
             Start.UseVisualStyleBackColor = false;
             // 
-            // Carpathian
-            // 
-            Carpathian.AutoSize = true;
-            Carpathian.BackColor = Color.Transparent;
-            Carpathian.Font = new Font("Pixeled", 30F);
-            Carpathian.ForeColor = SystemColors.ButtonFace;
-            Carpathian.Location = new Point(21, 21);
-            Carpathian.Name = "Carpathian";
-            Carpathian.Size = new Size(529, 140);
-            Carpathian.TabIndex = 1;
-            Carpathian.Text = "Carpathian";
-            // 
-            // star
-            // 
-            star.AutoSize = true;
-            star.BackColor = Color.Transparent;
-            star.Font = new Font("Pixeled", 30F);
-            star.ForeColor = SystemColors.ButtonFace;
-            star.Location = new Point(381, 115);
-            star.Name = "star";
-            star.Size = new Size(259, 140);
-            star.TabIndex = 2;
-            star.Text = "star";
-            // 
             // Profile
             // 
+            Profile.BackColor = Color.OliveDrab;
+            Profile.BackgroundImage = (Image)resources.GetObject("Profile.BackgroundImage");
+            Profile.BackgroundImageLayout = ImageLayout.Stretch;
             Profile.Font = new Font("Pixeled", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            Profile.Location = new Point(965, 12);
+            Profile.Location = new Point(961, 12);
             Profile.Name = "Profile";
-            Profile.Size = new Size(51, 52);
+            Profile.Size = new Size(55, 50);
             Profile.TabIndex = 3;
-            Profile.Text = "P";
             Profile.TextAlign = ContentAlignment.TopRight;
-            Profile.UseVisualStyleBackColor = true;
+            Profile.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(33, 37);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(741, 251);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
             // 
             // EnterWindow
             // 
@@ -92,23 +81,21 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(1041, 643);
+            Controls.Add(pictureBox1);
             Controls.Add(Profile);
-            Controls.Add(Carpathian);
-            Controls.Add(star);
             Controls.Add(Start);
             FormBorderStyle = FormBorderStyle.None;
             Name = "EnterWindow";
             Text = "EnterWindow";
             Load += EnterWindow_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Button Start;
-        private Label Carpathian;
-        private Label star;
         private Button Profile;
+        private PictureBox pictureBox1;
     }
 }
