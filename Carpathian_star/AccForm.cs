9eb0f9e25fc63b5496c1021fce838192896c26cc
Bodnar Anastasiia <bodnar@gmail.com>
2024-11-06 18:24:08 +0200

    -

--- Carpathian_star/AccForm.cs
+++ Carpathian_star/AccForm.cs
@@ -19,6 +19,7 @@ namespace Carpathian_star
         EnterWindow enterWindow;
         public AccForm(EnterWindow form)
         {
+            //
             InitializeComponent();
             Program.ApplyCustomFont(this.Controls);
 
